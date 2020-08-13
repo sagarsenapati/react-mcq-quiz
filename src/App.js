03bc7caa87d73c4f0a5145063052e9fa8c61067f
Sudipta Sengupta <sudiptasg87@users.noreply.github.com>
2020-08-13 09:25:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Question from './components/Question';
+
+function App() {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Quiz</h2>
+        </div>
+        {this.state.result ? this.renderResult() : this.renderQuiz()}
+      </div>
+    );
+  }
+}
+
+export default App;
